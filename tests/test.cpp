4248b061e71f81ba0e1fc8f44fc39af36fc01b2f
Junghoon Choi <61895947+be9904@users.noreply.github.com>
2023-11-26 09:22:36 +0000

    11382

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 
-#include "foo.h"
-#include "bar.h"
-
 using namespace std;
 
 int main(){
-    // foo();
-    // bar();
-    int x, y, z;
+    long int x, y, z;
     cin >> x >> y >> z;
     cout << x+y+z << endl;
     return 0;
