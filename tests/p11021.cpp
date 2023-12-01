bd45cbfc51aa2d7e10e68841afef6dfae1b6853c
Junghoon Choi <61895947+be9904@users.noreply.github.com>
2023-12-01 13:40:15 +0000

    11021

---
+++ tests/p11021.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int cases;
+    cin >> cases;
+
+    int ab[2 * cases];
+    for(int i = 0 ; i < 2 * cases ; i += 2){
+        cin >> ab[i] >> ab[i+1];
+    }
+
+    for(int i = 0 ; i < 2 * cases ; i += 2){
+        cout << "Case #" << (i+2)/2 << ": " 
+        << ab[i] + ab[i+1] << endl;
+    }
+
+    return 0;
+}
