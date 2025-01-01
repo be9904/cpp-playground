2d76844e7661ed8cfcc1e24f1c71b1b9d266ff38
Junghoon Choi <61895947+be9904@users.noreply.github.com>
2025-01-01 08:15:31 +0000

    added more lines to namespace.cpp

--- profcpp/ch1/namespace.cpp
+++ profcpp/ch1/namespace.cpp
@@ -3,13 +3,30 @@
 // this is how a namespace is declared.
 namespace mycode {
     void foo(){
-        std::cout << "foo() called in \'mycode\' namespace" << std::endl;
+        std::cout << "foo() called in \'mycode\' namespace." << std::endl;
+    }
+}
+
+namespace mycode::mynestedcode{
+    void foo(){
+        std::cout << "foo() called in \'mynestedcode\' namespace in \'mycode\' namespace." << std::endl;
     }
 }
 
 int main(){
     // foo() in namespace mycode can be declared using the scope resolution operator '::'.
     mycode::foo();
+    mycode::mynestedcode::foo();
+
+    // declaring the usage of namespace std with the 'using' keyword
+    // cout << "" << endl; // this will not execute because it is out of the std namespace scope.
+    using namespace std;
+    cout << endl << "we can print a line by defining the std namespace scope with the \'using\' keyword." << endl;
+
+    // declaring a namespace alias to give a shorter name
+    namespace NestedNamespace = mycode::mynestedcode;
+    cout << endl << "The line below is printed using a namespace alias." << endl;
+    NestedNamespace::foo();
 
     return 0;
 }
